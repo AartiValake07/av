d1df888aee8a39d7aef91fb64538aaa7dac5d8ba
Saurabh Lohar <saurabhlohar176@gmail.com>
2025-07-23 13:00:10 +0530

    First commit

---
+++ ojt-react-app/src/App.jsx
@@ -0,0 +1,54 @@
+// src/App.jsx
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer'; // <--- UPDATED PATH HERE
+
+import Home from './pages/Home';
+import About from './pages/About';
+import Courses from './pages/Courses';
+import Contact from './pages/Contact';
+import Admissions from './pages/Admissions';
+
+import './index.css';
+import ChatbotComponent from './components/Chatbot/ChatbotComponents';
+import DeveloperInfoPopup from './components/DeveloperInfo/DeveloperInfoPopup';
+
+
+const App = () => {
+  const [showPopup, setShowPopup] = useState(true);
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  };
+  return (
+    <>
+    <div>
+        {/* Your main application content */}
+        <DeveloperInfoPopup
+          show={showPopup}
+          onClose={handleClosePopup}
+          studentName="Arti Valake"
+          studentPhotoUrl="/images/Arti.jpg"// Path to their photo
+          uniqueMessage="Learned so much during this OJT! This app showcases my independent coding and deployment skills"
+        />
+      </div>
+    <Router>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/courses" element={<Courses />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/admissions" element={<Admissions />} />
+        </Routes>
+      </main>
+      <ChatbotComponent />
+      <Footer />
+    </Router>
+    </>
+  );
+
+}
+
+export default App;
